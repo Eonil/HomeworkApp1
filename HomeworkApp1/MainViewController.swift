fc7c1443359a618d80bd6633a053664e195d9835
Hoon H <drawtree@me.com>
2015-02-26 11:02:37 +0900

    Initial import.

---
+++ HomeworkApp1/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  HomeworkApp1
+//
+//  Created by Hoon H. on 2015/02/26.
+//
+//
+
+import Foundation
+import UIKit
+
+final class MainViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.view.backgroundColor	=	UIColor.whiteColor()
+	}
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+	}
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+	}
+}
---
+++ HomeworkApp1/SlideShowViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SlideShowViewController.swift
+//  HomeworkApp1
+//
+//  Created by Hoon H. on 2015/02/26.
+//
+//
+
+import Foundation
